33329828acc5d33255ba4d91f1fcef412a158111
awoabdi <awoabdi20@gmail.com>
2025-05-22 01:17:19 -0400

    Update AggressiveStrategy.java

--- src/main/java/ca/yorku/cmg/lob/stockexchange/tradingagent/src/main/java/ca/yorku/cmg/lob/stockexchange/tradingagent/AggressiveStrategy.java
+++ src/main/java/ca/yorku/cmg/lob/stockexchange/tradingagent/src/main/java/ca/yorku/cmg/lob/stockexchange/tradingagent/AggressiveStrategy.java
@@ -1,1 +1,16 @@
+package ca.yorku.cmg.lob.stockexchange.tradingagent;
+
+import ca.yorku.cmg.lob.stockexchange.events.Event;
+
+public class AggressiveStrategy implements ITradingStrategy {
+
+    @Override
+    public void actOnEvent(Event e, int pos, int price) {
+        if (e.isGoodNews()) {
+            System.out.println("Aggressive strategy: this should buy more stock due to good news.");
+        } else {
+            System.out.println("Aggressive strategy: it will sell large position due to bad news.");
+        }
+    }
+}
 
