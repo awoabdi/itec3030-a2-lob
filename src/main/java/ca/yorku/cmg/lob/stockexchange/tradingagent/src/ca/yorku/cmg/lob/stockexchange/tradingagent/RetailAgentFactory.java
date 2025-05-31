8eb4b6936017365490d19396ff97def5c7a63138
awoabdi <awoabdi20@gmail.com>
2025-05-30 21:00:27 -0400

    Create RetailAgentFactory.java

---
+++ src/main/java/ca/yorku/cmg/lob/stockexchange/tradingagent/src/ca/yorku/cmg/lob/stockexchange/tradingagent/RetailAgentFactory.java
@@ -0,0 +1,13 @@
+package ca.yorku.cmg.lob.stockexchange.tradingagent;
+
+import ca.yorku.cmg.lob.stockexchange.StockExchange;
+import ca.yorku.cmg.lob.stockexchange.events.NewsBoard;
+import ca.yorku.cmg.lob.trader.Trader;
+
+public class RetailAgentFactory implements TradingAgentFactory {
+
+    @Override
+    public TradingAgent createTradingAgent(Trader t, StockExchange e, NewsBoard n) {
+        return new RetailTradingAgent(t, e, n);
+    }
+}
